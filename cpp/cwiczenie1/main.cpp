4d01db5982c6d2490ad198ed24a4e748eae7c46a
boxeggg <student@student.machine>
2024-10-02 06:29:44 +0000

    studentmachine send

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
+\#include <iostream>
 
 using namespace std;
 
-int main(){
-	cout<<"hello world 1"<<endl;
-	return 0;
+void BubbleSort(int arr[10]){
+	for(int i=0; i<10 - 1; i++){
+		for(int j=0; j<10 - 1 - i; j++){
+			if(arr[j] > arr[j + 1]){
+				std::swap(arr[j],arr[j+1]);
+			}
+		}
+
+	}
 }
+void PrintArray(int arr[10]){
+	for(int i=0; i<10; i++){
+		std::cout << arr[i] << " ";
+	}
+
+	std::cout << std::endl;
+}
+int main(){
+	int arr[10] = {64,34,25,12,22,11,90,45,67,18};
+	PrintArray(arr);
+	BubbleSort(arr);
+	std::cout <<  "Posortowane" << std::endl;
+	PrintArray(arr);
+
+}
+
