65c716ee24926604616d99e8a3ddd0e033d9f8cd
Kamil Szydłowski <102488230+boxeggg@users.noreply.github.com>
2024-10-07 19:15:16 +0200

    Update bubble sort from array to vector

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -1,30 +1,18 @@
-\#include <iostream>
+#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void BubbleSort(int arr[10]){
-	for(int i=0; i<10 - 1; i++){
-		for(int j=0; j<10 - 1 - i; j++){
-			if(arr[j] > arr[j + 1]){
-				std::swap(arr[j],arr[j+1]);
+void bubbleSort(vector<int> &v) {
+	int n = v.size();
+
+	for (int i = 0; i < n - 1; i++){
+		for(int j = 0; j < n - 1; j++){
+			if(v[j] > v[j+1]){
+				int temp = v[j];
+				v[j] = v[j+1];
+				v[j+1] = temp;
 			}
 		}
-
 	}
 }
-void PrintArray(int arr[10]){
-	for(int i=0; i<10; i++){
-		std::cout << arr[i] << " ";
-	}
-
-	std::cout << std::endl;
-}
-int main(){
-	int arr[10] = {64,34,25,12,22,11,90,45,67,18};
-	PrintArray(arr);
-	BubbleSort(arr);
-	std::cout <<  "Posortowane" << std::endl;
-	PrintArray(arr);
-
-}
-
